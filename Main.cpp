ebd3b6e834e5ad01384ef048c506d78323d45867
yusuke takaoka <m1716@s.akashi.ac.jp>
2018-09-11 10:47:23 +0900

    diff_time

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<vector>
 #include<random>
-#include<string>
 #include<sstream>
+#include<cstdlib>
+#include<ctime>
 #include<opencv2/opencv.hpp>
 
 using namespace cv;
@@ -10,87 +11,112 @@ using namespace std;
 
 int main(void){
 
-	Mat frame;
+	Mat frame = imread("cap.jpeg");
+	Mat hsv;
+	Mat binary;
+	Mat src;
+	unsigned long int pre_t;
+	unsigned long int now_t;
+	unsigned long int diff_t;
+	bool flag = false;
 
-	VideoCapture cap(3);
-    	if(!cap.isOpened()){
+	VideoCapture cap(0);
+	if(!cap.isOpened()){
 		cout <<"Not Opened"<< endl;
-       		return -1;
-    	}else{
+		return -1;
+	}else{
 		cout <<"Open Successful"<<endl;
 	}
 
-	int key = waitKey(1);
+	pre_t = time_t();
 
-        if(key == 115){
-		cap >> frame;
-		imwrite("sample.jpeg", frame);
-    		//グレースケール入力 膨張処理
-    		Mat src = imread("sample.jpeg", IMREAD_GRAYSCALE);
-    		dilate(src,src,Mat(),Point(-1,-1),3);
+	while(true){
+		now_t = time_t();
+		diff_t = now_t - pre_t;
+		if(flag = false || diff_t >= 4000){
+			cap >> frame;
+			imwrite("sample.jpeg", frame);
 
-    		//ラべリング処理
-    		Mat LabelImg;
-    		Mat stats;
-    		Mat centroids;
-    		int nLab = connectedComponentsWithStats(src, LabelImg, stats, centroids);
+			//グレースケール入力 膨張処理 ガウシアンフィルタ（ぼかし）メディアンフィルタ
+			GaussianBlur(frame, frame, cv::Size(11,3), 80, 3);
+			cvtColor(frame,hsv, COLOR_BGR2GRAY);
+			inRange(hsv,Scalar(0,0,200),Scalar(200,255,255),src);
+			medianBlur(src,src,1);
+			dilate(src,src,Mat(),Point(-1,-1),9);
+			erode(src,src,Mat(),Point(-1,-1),10);
+			dilate(src,src,Mat(),Point(-1,-1),5);
+			Mat neg = ~src;
 
-    		// ラベリング結果の描画色を決定
-    		vector<Vec3b> colors(nLab);
-    		colors[0] = Vec3b(0, 0, 0);
-    		for (int i = 1; i < nLab; ++i) {
-        		colors[i] = Vec3b((rand() & 255), (rand() & 255), (rand() & 255));//色をランダムで決定する（255で範囲指定しないといけないからビット演算）
-   	 	}	
 
-    		// ラベリング結果の描画
-    		Mat Dst(src.size(), CV_8UC3);
-    		for (int i = 0; i < Dst.rows; ++i) {
-        		int *lb = LabelImg.ptr<int>(i);
-        		Vec3b *pix = Dst.ptr<Vec3b>(i);
-        		for (int j = 0; j < Dst.cols; ++j) {
-            			pix[j] = colors[lb[j]];
-      			}	
-    		}
+			//ラべリング処理
+			Mat LabelImg;
+			Mat stats;
+			Mat centroids;
+			int nLab = connectedComponentsWithStats(neg, LabelImg, stats, centroids);
 
-    		//ROIの設定
-    		for (int i = 1; i < nLab; ++i) {
-        		int *param = stats.ptr<int>(i);
+			// ラベリング結果の描画色を決定
+			vector<Vec3b> colors(nLab);
+			colors[0] = Vec3b(0, 0, 0);
+			for (int i = 1; i < nLab; ++i) {
+				colors[i] = Vec3b((rand() & 255), (rand() & 255), (rand() & 255));//色をランダムで決定する（255で範囲指定しないといけないからビット演算）
+			}	
 
-        		int x = param[ConnectedComponentsTypes::CC_STAT_LEFT];
-        		int y = param[ConnectedComponentsTypes::CC_STAT_TOP];
-        		int height = param[ConnectedComponentsTypes::CC_STAT_HEIGHT];
-        		int width = param[ConnectedComponentsTypes::CC_STAT_WIDTH];
+			// ラベリング結果の描画
+			Mat Dst(neg.size(), CV_8UC3);
+			for (int i = 0; i < Dst.rows; ++i) {
+				int *lb = LabelImg.ptr<int>(i);
+				Vec3b *pix = Dst.ptr<Vec3b>(i);
+				for (int j = 0; j < Dst.cols; ++j) {
+					pix[j] = colors[lb[j]];
+				}	
+			}
 
-        		rectangle(Dst, Rect(x, y, width, height), Scalar(0, 255, 0), 2);
-    		}
+			//ROIの設定
+			for (int i = 1; i < nLab; ++i) {
+				int *param = stats.ptr<int>(i);
 
-    		//重心の出力
-    		for (int i = 1; i < nLab; ++i) {
-        		double *param = centroids.ptr<double>(i);
-        		int x = static_cast<int>(param[0]);
-        		int y = static_cast<int>(param[1]);
+				int x = param[ConnectedComponentsTypes::CC_STAT_LEFT];
+				int y = param[ConnectedComponentsTypes::CC_STAT_TOP];
+				int height = param[ConnectedComponentsTypes::CC_STAT_HEIGHT];
+				int width = param[ConnectedComponentsTypes::CC_STAT_WIDTH];
 
-        		circle(Dst,cv::Point(x, y), 3, cv::Scalar(0, 0, 255), -1);
-    		}
+				rectangle(Dst, Rect(x, y, width, height), Scalar(0, 255, 0), 2);
+			}
 
-    		//面積値の出力
-    		for (int i = 1; i < nLab; ++i) {
-        		int *param = stats.ptr<int>(i);
-        		cout << "area "<< i <<" = " << param[ConnectedComponentsTypes::CC_STAT_AREA] << endl;
+			//重心の出力
+			int centerX[nLab];
+			int centerY[nLab];
+			for (int i = 1; i < nLab; ++i) {
+				double *param = centroids.ptr<double>(i);
+				centerX[i] = static_cast<int>(param[0]);
+				centerY[i] = static_cast<int>(param[1]);
 
-        		//ROIの左上に番号を書き込む
-        		int x = param[cv::ConnectedComponentsTypes::CC_STAT_LEFT];
-        		int y = param[cv::ConnectedComponentsTypes::CC_STAT_TOP];
-        		stringstream num;
-        		num << i;
-        		putText(Dst, num.str(), Point(x+5, y+20), FONT_HERSHEY_COMPLEX, 0.7, Scalar(0, 255, 255), 2);
-    		}
+				circle(Dst,cv::Point(centerX[i],centerY[i]), 3, cv::Scalar(0, 0, 255), -1);
+				cout << i <<" "<< "x:" << centerX[i] << "  y:"<< centerY[i] << endl;
+			}
 
-    		imshow("Src", src);
-    		imshow("にゃーん", Dst);
-    		waitKey();
-	}else if(key == 113){
-		destroyAllWindows();
+			//面積値の出力
+			for (int i = 1; i < nLab; ++i) {
+				int *param = stats.ptr<int>(i);
+				cout << "area "<< i <<" = " << param[ConnectedComponentsTypes::CC_STAT_AREA] << endl;
+
+				//ROIの左上に番号を書き込む
+				int x = param[cv::ConnectedComponentsTypes::CC_STAT_LEFT];
+				int y = param[cv::ConnectedComponentsTypes::CC_STAT_TOP];
+				stringstream num;
+				num << i;
+				putText(Dst, num.str(), Point(x+5, y+20), FONT_HERSHEY_COMPLEX, 0.7, Scalar(0, 255, 255), 2);
+			}
+
+			flag = 1;
+			pre_t = pre_t + now_t;
+			imshow("frame",frame);
+			imshow("Src", src);
+			imshow("にゃーん", Dst);
+			waitKey();
+
+		}
 	}
-    		return 0;
+
+	return 0;
 }
