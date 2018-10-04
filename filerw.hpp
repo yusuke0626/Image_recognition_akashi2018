792c0dd8b3da7ff869f10a0564868a1f94c2b1f4
yusuke <m1716@s.akashi.ac.jp>
2018-10-04 12:22:29 +0000

    file

---
+++ filerw.hpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+void AccessPointConnect(void){
+    system("ssh pi@akashiaspoint.local");
+    system("AkashiRoboB");
+    return 0;
+}
+
+void sendFile(string filename){
+    string scp("sshpass -p 'a' scp ");
+    string adr(" pi@tapioka.local:/home/pi/Image_recognition_akashi2018");
+    string putcommand = scp + filename + adr;
+    const char* cstr = putcommand.c_str();
+    system(cstr);
+    return 0;
+}
+
+int readFile(string readfilename){
+    ifstream servefile(readfilename);
+    string tabledata;
+
+    if (servefile.fail()) {
+        cerr << "Failed to open file." << endl;
+        return -1;
+    }
+    int a[5];
+    while (getline(servefile,tabledata)) {
+        int i = 0;
+        ++i;
+        //cout << tabledata << endl;
+	const char* num = tabledata.c_str();
+        a[i] = atoi(num);
+        cout << a[i] << endl;
+    }
+    return 0;
+}
+
+
