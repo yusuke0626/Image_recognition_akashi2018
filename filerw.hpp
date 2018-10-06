75ddf814f780bf8766a872c153ffe37d20564f92
yusuke <m1716@s.akashi.ac.jp>
2018-10-06 13:24:50 +0900

    dekitaa

--- filerw.hpp
+++ filerw.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include<iostream>
 #include<cstdlib>
 #include<string>
@@ -5,37 +7,42 @@
 
 using namespace std;
 
-void AccessPointConnect(void){
-    system("ssh pi@akashiaspoint.local");
-    system("AkashiRoboB");
-}
-
-void sendFile(string filename){
-    string scp("sshpass -p 'a' scp ");
-    string adr(" pi@tapioka.local:/home/pi/Image_recognition_akashi2018");
-    string putcommand = scp + filename + adr;
-    const char* cstr = putcommand.c_str();
-    system(cstr);
-}
-
-int readFile(string readfilename){
-    ifstream servefile(readfilename);
-    string tabledata;
-
-    if (servefile.fail()) {
-        cerr << "Failed to open file." << endl;
-        return -1;
+namespace TD{
+    void Connect(void){
+        system("ssh pi@akashiaspoint.local");
+        system("AkashiRoboB");
     }
-    int a[5];
-    int i = 0;
-    while (getline(servefile,tabledata)) {
-        ++i;
-        //cout << tabledata << endl;
-	const char* num = tabledata.c_str();
-        a[i] = atoi(num);
-        cout << i << ":" <<a[i] << endl;
-    }
-    return 0;
-}
 
+    void send(string filename){
+        string scp("sshpass -p 'a' scp ");
+        string adr(" pi@tapioka.local:/home/pi/Image_recognition_akashi2018");
+        string putcommand = scp + filename + adr;
+        const char* cstr = putcommand.c_str();
+        system(cstr);
+    }
+
+    int read(string readfilename,int sendPlace[3]){
+        ifstream servefile(readfilename);
+        string tabledata;
+
+        if (servefile.fail()) {
+            cerr << "Failed to open file." << endl;
+            return -1;
+        }
+
+        int i = 0;
+        int TablePlace[3];
+        while (getline(servefile,tabledata)) {
+            //cout << tabledata << endl;
+            const char* num = tabledata.c_str();
+            TablePlace[i] = atoi(num);
+            sendPlace[i] = TablePlace[i];
+            cout << i << ":" <<TablePlace[i] << endl;//TablePlaceは関数外からも使用可能
+            i = i+1;
+        }
+        return 0;
+    }
+
+
+}
 
