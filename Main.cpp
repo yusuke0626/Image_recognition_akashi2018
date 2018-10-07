0baee78272c36db509327b43fe20c2233970b7e9
yusuke <m1716@s.akashi.ac.jp>
2018-10-07 16:42:19 +0900

    1times

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,23 @@
-#include<iostream>
-#include<cstdio>
-#include<vector>
-#include<random>
-#include<sstream>
-#include<ctime>
-#include<opencv2/opencv.hpp>
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <random>
+#include <sstream>
+#include <vector>
+#include "filerw.hpp"
 
-#define X_ORIGIN 130
-#define Y_ORIGIN 50
-#define X_SIZE 120
-#define Y_SIZE 400
-#define FRAME_X_SIZE 135
-#define FRAME_Y_SIZE 400
+#define X_ORIGIN 80
+#define Y_ORIGIN 0
+#define X_SIZE 200
+#define Y_SIZE 460
+#define FRAME_X_SIZE 200
+#define FRAME_Y_SIZE 460
 
 using namespace cv;
 using namespace std;
 
-int main(void){
+int main(void) {
 
     time_t pre_t;
     time_t now_t;
@@ -30,14 +31,12 @@ int main(void){
     unsigned int e = 255;
     unsigned int f = 255;
 
-
-    VideoCapture cap(1);
-    if(!cap.isOpened()){
-
-        cout <<"Not Opened"<< endl;
+    VideoCapture cap("video.webm");
+    if (!cap.isOpened()) {
+        cout << "Not Opened" << endl;
         return -1;
-    }else{
-        cout <<"Open Successful"<<endl;
+    } else {
+        cout << "Open Successful" << endl;
     }
 
     while (true) {
@@ -47,58 +46,55 @@ int main(void){
         cap >> img;
         cap >> frame;
         Mat rsi(img, Rect(X_ORIGIN, Y_ORIGIN, X_SIZE, Y_SIZE));;
-        resize(rsi,rsi, Size(FRAME_X_SIZE, FRAME_Y_SIZE));
-        imshow("img",rsi);
+        resize(rsi, rsi, Size(FRAME_X_SIZE, FRAME_Y_SIZE));
+        imshow("img", rsi);
 
         int key = waitKey(250);
         bool swflag = false;
 
-        switch (key){
+        switch (key) {
             case 113:
-                a = a + 5; break;
+                a = a + 5;break;
             case 97:
-                a = a - 3; break;
+                a = a - 3;break;
             case 119:
-                b = b + 5; break;
+                b = b + 5;break;
             case 115:
-                b = b - 3; break;
+                b = b - 3;break;
             case 101:
-                c = c + 5; break;
+                c = c + 5;break;
             case 100:
-                c = c - 3; break;
+                c = c - 3;break;
             case 117:
-                d = d - 5; break;
+                d = d - 5;break;
             case 106:
-                d = d + 3; break;
+                d = d + 3;break;
             case 105:
-                e = e - 5; break;
+                e = e - 5;break;
             case 107:
-                e = e + 3; break;
+                e = e + 3;break;
             case 111:
-                f = f - 5; break;
+                f = f - 5;break;
             case 108:
-                f = f + 3; break;
+                f = f + 3;break;
             case 122:
-                swflag = true; break;
+                swflag = true;break;
         }
 
-
-        if(swflag == true){
+        if (swflag == true){
             break;
         }
-
-        printf("(%d,%d,%d)\n",a,b,c);
-        printf("(%d,%d,%d)\n",d,e,f);
+        printf("(%d,%d,%d)\n", a, b, c);
+        printf("(%d,%d,%d)\n", d, e, f);
         Mat dst(frame, Rect(X_ORIGIN, Y_ORIGIN, X_SIZE, Y_SIZE));;
         resize(dst, dst, Size(FRAME_X_SIZE, FRAME_Y_SIZE));
-        cvtColor(dst,hsv, COLOR_BGR2HSV);
-        inRange(hsv,Scalar(a,b,c),Scalar(d,e,f),frame);
-        erode(frame,frame,Mat(),Point(-1,-1),3);
-        dilate(frame,frame,Mat(),Point(-1,-1),5);
-        imshow("binary img",frame);
+        cvtColor(dst, hsv, COLOR_BGR2HSV);
+        inRange(hsv, Scalar(a, b, c), Scalar(d, e, f), frame);
+        erode(frame, frame, Mat(), Point(-1, -1), 3);
+        dilate(frame, frame, Mat(), Point(-1, -1), 5);
+        imshow("binary img", frame);
         waitKey(200);
     }
-
 
     destroyAllWindows();
     cout << "*---------Main Start---------*" << endl;
@@ -109,42 +105,38 @@ int main(void){
 
         Mat mainframe;
         Mat mainhsv;
-        //Mat maindst;
-        int stopkey = waitKey(200);
+        int stopkey = waitKey(1000);
 
-        if(stopkey == 97)
+        if (stopkey == 97)
             break;
 
         now_t = time(NULL);
         diff_t = now_t - pre_t;
 
-        if(flag = false || diff_t >= 4){
+        if (flag = false || diff_t >= 8) {
             cap >> mainframe;
-            Mat maindst(mainframe, Rect(X_ORIGIN, Y_ORIGIN, X_SIZE, Y_SIZE));;
+            Mat maindst(mainframe, Rect(X_ORIGIN, Y_ORIGIN, X_SIZE, Y_SIZE));
             resize(maindst, maindst, Size(FRAME_X_SIZE, FRAME_Y_SIZE));
-            cvtColor(maindst,mainhsv, COLOR_BGR2HSV);
-            inRange(mainhsv,Scalar(a,b,c),Scalar(d,e,f),maindst);
-            erode(maindst,maindst,Mat(),Point(-1,-1),3);
-            dilate(maindst,maindst,Mat(),Point(-1,-1),5);
+            cvtColor(maindst, mainhsv, COLOR_BGR2HSV);
+            inRange(mainhsv, Scalar(a, b, c), Scalar(d, e, f), maindst);
+            erode(maindst, maindst, Mat(), Point(-1, -1), 3);
+            dilate(maindst, maindst, Mat(), Point(-1, -1), 5);
             waitKey(200);
-            imshow("img",mainhsv);
-            //imshow("binary img",maindst);
+            imshow("img", mainhsv);
 
-
-            //ラべリング処理
+            //ラべリング
             Mat LabelImg;
             Mat stats;
             Mat centroids;
-            int nLab = connectedComponentsWithStats(maindst, LabelImg, stats, centroids);
+            int nLab =connectedComponentsWithStats(maindst, LabelImg, stats, centroids);
 
-            // ラベリング結果の描画色を決定
+            // 描画色決定
             vector<Vec3b> colors(nLab);
             colors[0] = Vec3b(0, 0, 0);
             for (int i = 1; i < nLab; ++i) {
-                colors[i] = Vec3b((rand() & 255), (rand() & 255), (rand() & 255));//色をランダムで決定する（255で範囲指定しないといけないからビット演算）
+                colors[i] = Vec3b((rand() & 255), (rand() & 255),(rand() &  255)); //色をランダムで決定する（255で範囲指定しないといけないからビット演算）
             }
-
-            // ラベリング結果の描画
+            //描画
             Mat Dst(maindst.size(), CV_8UC3);
             for (int i = 0; i < Dst.rows; ++i) {
                 int *lb = LabelImg.ptr<int>(i);
@@ -153,57 +145,49 @@ int main(void){
                     pix[j] = colors[lb[j]];
                 }
             }
-
-            //ROIの設定
+            // ROIの設定
             for (int i = 1; i < nLab; ++i) {
                 int *param = stats.ptr<int>(i);
-
                 int x = param[ConnectedComponentsTypes::CC_STAT_LEFT];
                 int y = param[ConnectedComponentsTypes::CC_STAT_TOP];
                 int height = param[ConnectedComponentsTypes::CC_STAT_HEIGHT];
                 int width = param[ConnectedComponentsTypes::CC_STAT_WIDTH];
-
                 rectangle(Dst, Rect(x, y, width, height), Scalar(0, 255, 0), 2);
             }
-
-            //重心の出力
+            //重心計算
             int centerX[nLab];
             int centerY[nLab];
             for (int i = 1; i < nLab; ++i) {
                 double *param = centroids.ptr<double>(i);
                 centerX[i] = static_cast<int>(param[0]);
                 centerY[i] = static_cast<int>(param[1]);
-
-                circle(Dst,Point(centerX[i],centerY[i]), 3, Scalar(0, 0, 255),-1);
+                circle(Dst, Point(centerX[i], centerY[i]), 3, Scalar(0, 0, 255), -1);
             }
 
-            for(int count = 1;count < nLab;++count)
-                cout<< count <<" "<< "x:" << centerY[count] << "  y:"<< centerX[count] << endl;
-
-            //面積値の出力
-            for (int i = 1; i < nLab; ++i) {
-                int *param = stats.ptr<int>(i);
-                cout << "area "<< i <<" = " << param[ConnectedComponentsTypes::CC_STAT_AREA] << endl;
-
-                //ROIの左上に番号を書き込む
-                int x = param[ConnectedComponentsTypes::CC_STAT_LEFT];
-                int y = param[ConnectedComponentsTypes::CC_STAT_TOP];
-                stringstream num;
-                num << i;
-                putText(Dst, num.str(), Point(x+5, y+20), FONT_HERSHEY_COMPLEX, 0.7, Scalar(0, 255, 255), 2);
-            }
-
-            flag = true;
-            pre_t = now_t;
-            printf("%d,%d,%d,%d,%d,%d\n",a,b,c,d,e,f);
-            //imshow("frame",mainframe);
-            //imshow("Src", src);
-            imshow("にゃーん", Dst);
-            waitKey(200);
+            int tableXpoint[3] = {0,0,0};
+            int tableYpoint[3] = {0,0,0};
+            for (int count = 1; count < 2/*nLab*/; ++count)
+                //座標
+                for (int i = 1; i < nLab; ++i) {
+                    int *param = stats.ptr<int>(i);
+                    // 左上に番号を書き込む
+                    tableXpoint[i-1] = param[ConnectedComponentsTypes::CC_STAT_LEFT];
+                    tableYpoint[i-1] = param[ConnectedComponentsTypes::CC_STAT_TOP];
+                    stringstream num;
+                    num << i;
+                    putText(Dst, num.str(), Point(tableXpoint[i-1] + 5,tableYpoint[i-1] + 20), FONT_HERSHEY_COMPLEX,0.7, Scalar(0, 255, 255), 2);
+                    cout << i << " "<< "x:" << tableXpoint[i-1] << "  y:" << tableYpoint[i-1] << endl;
+                }
+            FRW::write("tabledata.txt",tableXpoint);
+            FRW::send("tabledata.txt");
+            flag = false;//true;
+            //pre_t = now_t;
+            printf("%d,%d,%d,%d,%d,%d\n", a, b, c, d, e, f);
+            //imshow("にゃーん", Dst);
+            waitKey(1000);
+            break;
         }
     }
     cout << "*----------Main finish-----------*" << endl;
     return 0;
 }
-
-
